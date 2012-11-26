e8382d7b3f96e21b9eb0be7404d8f81592c1fe0f
Leo Keselman <Chipper10@gmail.com>
2012-11-25 21:53:33 -0500

    Added parseSentence function.
    Returns ambiguity classes for each word.

--- POS_Tagger.py
+++ POS_Tagger.py
@@ -1,4 +1,7 @@
+#for dictionary loading
 import pickle
+#natural language toolkit
+import nltk
 
 POS_dict = pickle.load( open( "POSdict.p", "rb" ) )
 
@@ -7,3 +10,18 @@ def get_POS(word):
         return POS_dict[word]
     else:
         return 'no POS'
+
+def parse_Sentence(sentence):
+    POSs = []
+    #tokenize
+    words = nltk.word_tokenize(sentence)
+    #get POSs (parts of speech)
+    for word in words:
+        if word == '.':
+            POSs.append('x')
+        else:
+            word = word.lower()
+            POSs.append(get_POS(word))
+    return POSs
+        
+        
